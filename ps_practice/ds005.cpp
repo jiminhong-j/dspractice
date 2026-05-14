80894ac622f3e71665a34ad4018af3c78cbf9ced
Jimin <jiminh101@gmail.com>
2026-05-14 22:48:03 +0900

    ds005

--- ps_practice/ds005.cpp
+++ ps_practice/ds005.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+using namespace std;
 
 int main(){
-    string input;
+    string str;
+
+    // 입력
+    getline(cin, str);
+
+    int* alphabet = new int[26];
+
+    for(int i = 0; i < 26; i++){
+        alphabet[i] = 0;
+    }
+
+    int blanks = 0;
+
+    for(int i = 0; i < str.length(); i++){
+        if(str[i] == ' '){
+            blanks++;
+        } else if(isalpha(str[i])){
+            char c = toupper(str[i]);
+            alphabet[c - 'A']++;
+        }
+    }
+
+
+    // 출력
+    cout << "blanks : " << blanks << endl;
     
+    for(int i = 0; i < 26; i++){
+        if(alphabet[i] > 0){
+            cout << char(i + 'A') << " : " << alphabet[i] << endl;
+        }
+    }
+
+
+    delete[] alphabet;
+    return 0;
 }
